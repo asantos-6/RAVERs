78b86b9f45f9bfabd4e77ed8e78b344fc13a4a9d
francescopapaleo <francesco.papaleo@gmail.com>
2023-12-11 20:44:56 +0100

    neutone export notebook, pre-trained models, gitignore and git-lfs

---
+++ models/GMDrums_v3_29-09_3M_streaming.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8d621d494a983f9500ee476e79de5041dd01be3835502d1ce6c4409c59f4ef5f
+size 145863264
---
+++ models/darbouka_onnx.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2631209242a90aaeca4c1c2985b1b104595c60713b44fef0a1c1e49b8e995f98
+size 26350036
